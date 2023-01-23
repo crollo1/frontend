e7a1614e28bde0b31e55c3a7de3d3fd241f8c06b
Craig Rolleston <craig.rolleston@gmail.com>
2023-01-23 10:51:41 +1000

    initialise frontend

---
+++ src/pages/chakra/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+};
+
+export const theme = extendTheme(
+  { config },
+  {
+    colors: {
+        brand: {
+            100: "#3d84f7",
+        },
+    },
+    styles: {
+        global: () => ({
+            body: {
+                bg: "blackAlpha.800",
+                color: "#4299e1",
+            },
+        }),
+    },
+  }
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import { Box } from "@chakra-ui/react";
+import { NextPageContext } from "next";
+import { getSession, useSession } from "next-auth/react";
+import Auth from "./components/Auth/Auth";
+import Chat from "./components/Chat/Chat";
+import { Session } from "next-auth";
+
+
+export default function Home() {
+
+  const { data: session } = useSession();
+  console.log("Here is the SESSION!", session);
+
+  const reloadSession = () => {};
+  
+  return (
+
+    <Box >
+      {session?.user?.username ? (
+        <Chat /> 
+      ) : ( 
+        <Auth session={session} reloadSession={reloadSession} />
+      )}
+    </Box>
+
+    // <div>
+    //   {data?.user ? (
+    //     <button onClick={() => signOut()}>Sign Out</button>
+    //   ):(
+    //     <button onClick={() => signIn('google')}>Sign In</button>
+    //   )}
+    //   {data?.user?.name}
+    // </div>
+  );
+
+};
+
+
+// This function below prevents the brief display of sign in button even though you are signed in
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
+
